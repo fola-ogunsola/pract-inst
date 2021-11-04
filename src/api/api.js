084d229640e7c650e0097a187ef364c46361240e
ammduncan <ammielgyanyawson@gmail.com>
2021-11-04 12:32:37 +0000

    replaced env variable

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: process.env.VUE_APP_INSTRAIL_API_URL,
+  baseURL: process.env.VUE_APP_INSTRAIL_ADMIN_API_URL,
   crossdomain: true,
   timeout: 10000,
   headers: { "Content-Type": "application/json" }
